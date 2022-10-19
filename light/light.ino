d110ac6bcb7f8ab27e0897e90cfc55efffea71c4
wendazhi <121675004@qq.com>
2022-10-19 21:50:39 +0800

    shift+option+F 格式化代码1

--- light/light.ino
+++ light/light.ino
@@ -15,7 +15,7 @@
 
 // WiFi
 const char *ssid = "Xiaomi_F3EE_2.4G"; // Enter your WiFi name
-const char *password = "b5z0ubf4";  // Enter WiFi password
+const char *password = "b5z0ubf4";     // Enter WiFi password
 
 // MQTT Broker
 const char *mqtt_broker = "192.168.50.210";
@@ -25,15 +25,16 @@ const char *mqtt_username = "hass";
 const char *mqtt_password = "1234";
 const int mqtt_port = 1883;
 
-char switch_open[] = "ON";  // light ON
-char switch_close[] = "OFF";  // light OFF
+char switch_open[] = "ON";   // light ON
+char switch_close[] = "OFF"; // light OFF
 
 Ticker ticker;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
@@ -45,67 +46,82 @@ void setup() {
   // connecting to a WiFi network
   WiFi.begin(ssid, password);
   WiFi.setAutoReconnect(true);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.println("Connecting to WiFi..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
-  //connecting to a mqtt broker
+  // connecting to a mqtt broker
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
   reconnect_mqtt();
   ticker.attach(10, sayHi);
 }
 
-
-void sayHi(){
+void sayHi()
+{
   int status = digitalRead(SWITCH);
   Serial.printf("switch-%d", status);
-   Serial.println();
-  if(status == SWITCH_ON) {
+  Serial.println();
+  if (status == SWITCH_ON)
+  {
     client.publish(topic_status, switch_open);
-  } else if(status == SWITCH_OFF) {
+  }
+  else if (status == SWITCH_OFF)
+  {
     client.publish(topic_status, switch_close);
   }
 }
 
-void reconnect_mqtt(){
-  while (!client.connected()) {
-      String client_id = "esp8266-client-";
-      client_id += String(WiFi.macAddress());
-      Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-      if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-          Serial.println("Public emqx mqtt broker connected");
-          // publish and subscribe
-          client.publish(topic, "hello emqx");
-          client.subscribe(topic);
-      } else {
-          Serial.print("failed with state ");
-          Serial.print(client.state());
-          delay(2000);
-      }
+void reconnect_mqtt()
+{
+  while (!client.connected())
+  {
+    String client_id = "esp8266-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
+    {
+      Serial.println("Public emqx mqtt broker connected");
+      // publish and subscribe
+      client.publish(topic, "hello emqx");
+      client.subscribe(topic);
+    }
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
   }
 }
 
-void callback(char *topic, byte *payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
   Serial.print("Message:");
 
-    char payload_str[length] = { '\0' };
+  char payload_str[length] = {'\0'};
 
-    memcpy(payload_str, payload, length );
+  memcpy(payload_str, payload, length);
 
-    puts( payload_str );
-    Serial.println(payload_str);
+  puts(payload_str);
+  Serial.println(payload_str);
 
-  if(0 == strcmp(payload_str,switch_open)) {
+  if (0 == strcmp(payload_str, switch_open))
+  {
     digitalWrite(LED, LED_ON);
     digitalWrite(SWITCH, SWITCH_ON);
-  } else if(0 == strcmp(payload_str,switch_close)){
+  }
+  else if (0 == strcmp(payload_str, switch_close))
+  {
     digitalWrite(LED, LED_OFF);
     digitalWrite(SWITCH, SWITCH_OFF);
-  } else {
+  }
+  else
+  {
     return;
   }
 
@@ -113,8 +129,8 @@ void callback(char *topic, byte *payload, unsigned int length) {
   Serial.println("-----------------------");
 }
 
-void loop() {
+void loop()
+{
   client.loop();
   reconnect_mqtt();
 }
-
